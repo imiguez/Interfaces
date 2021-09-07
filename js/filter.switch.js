48903a59ca92252ff51bb7e44682d826ea3bcf7b
gaspitudai <gaspi.tudai@gmail.com>
2021-09-07 19:46:02 -0300

    sepia filter

--- js/filter.switch.js
+++ js/filter.switch.js
@@ -24,6 +24,7 @@ export default class FilterSwitch {
 
     drawSepiaFilter() {
         let imageData = this.#ctx.getImageData(this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5, this.#image.width, this.#image.height);
+<<<<<<< Updated upstream
         for (let x = 0; x < this.#ctx.canvas.width; x++) {
             for (let y = 0; y < this.#ctx.canvas.height; y++) {
                 // calculo el color gris del pixel
@@ -34,22 +35,38 @@ export default class FilterSwitch {
                 imageData.data[index + 2] = grey;
                 imageData.data[index + 3] = 255;
             }
+=======
+        let pixels = imageData.data;
+        let numPixels = imageData.width * imageData.height;
+        for (let i = 0; i < numPixels; i++) {
+            let r = pixels[i * 4];
+            let g = pixels[i * 4 + 1];
+            let b = pixels[i * 4 + 2];
+
+            pixels[i * 4] = 255 - r;
+            pixels[i * 4 + 1] = 255 - g;
+            pixels[i * 4 + 2] = 255 - b;
+
+            pixels[i * 4] = (r * .393) + (g * .769) + (b * .189);
+            pixels[i * 4 + 1] = (r * .349) + (g * .686) + (b * .168);
+            pixels[i * 4 + 2] = (r * .272) + (g * .534) + (b * .131);
+>>>>>>> Stashed changes
         }
         this.#ctx.putImageData(imageData, this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5);
     }
 
-    getRed(imageData, x, y) {
-        let index = (x + y * imageData.width) * 4;
-        return imageData.data[index + 0];
-    }
-    getGreen(imageData, x, y) {
-        let index = (x + y * imageData.width) * 4;
-        return imageData.data[index + 1];
-    }
-    getBlue(imageData, x, y) {
-        let index = (x + y * imageData.width) * 4;
-        return imageData.data[index + 2];
-    }
+    // getRed(imageData, x, y) {
+    //     let index = (x + y * imageData.width) * 4;
+    //     return imageData.data[index + 0];
+    // }
+    // getGreen(imageData, x, y) {
+    //     let index = (x + y * imageData.width) * 4;
+    //     return imageData.data[index + 1];
+    // }
+    // getBlue(imageData, x, y) {
+    //     let index = (x + y * imageData.width) * 4;
+    //     return imageData.data[index + 2];
+    // }
 
     drawNegativoFilter() {
         let imageData = this.#ctx.getImageData(this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5, this.#image.width, this.#image.height);
