a349c79481cee812d0e876bef3b61d087ddc2c50
gaspitudai <gaspi.tudai@gmail.com>
2021-09-07 19:35:13 -0300

    negativo-filter

--- js/filter.switch.js
+++ js/filter.switch.js
@@ -23,19 +23,19 @@ export default class FilterSwitch {
     }
 
     drawSepiaFilter() {
-        let dataImg = this.#ctx.getImageData(this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5, this.#image.width, this.#image.height);
+        let imageData = this.#ctx.getImageData(this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5, this.#image.width, this.#image.height);
         for (let x = 0; x < this.#ctx.canvas.width; x++) {
             for (let y = 0; y < this.#ctx.canvas.height; y++) {
                 // calculo el color gris del pixel
-                let grey = (this.getRed(dataImg, x, y) + this.getBlue(dataImg, x, y) + this.getGreen(dataImg, x, y)) / 3;
-                let index = (x + y * dataImg.width) * 4;
-                dataImg.data[index + 0] = grey;
-                dataImg.data[index + 1] = grey;
-                dataImg.data[index + 2] = grey;
-                dataImg.data[index + 3] = 255;
+                let grey = (this.getRed(imageData, x, y) + this.getBlue(imageData, x, y) + this.getGreen(imageData, x, y)) / 3;
+                let index = (x + y * imageData.width) * 4;
+                imageData.data[index + 0] = grey;
+                imageData.data[index + 1] = grey;
+                imageData.data[index + 2] = grey;
+                imageData.data[index + 3] = 255;
             }
         }
-        this.#ctx.putImageData(dataImg, this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5);
+        this.#ctx.putImageData(imageData, this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5);
     }
 
     getRed(imageData, x, y) {
@@ -51,7 +51,21 @@ export default class FilterSwitch {
         return imageData.data[index + 2];
     }
 
-    drawNegativoFilter() { }
+    drawNegativoFilter() {
+        let imageData = this.#ctx.getImageData(this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5, this.#image.width, this.#image.height);
+        let pixels = imageData.data;
+        let numPixels = imageData.width * imageData.height;
+        for (let i = 0; i < numPixels; i++) {
+            let r = pixels[i * 4];
+            let g = pixels[i * 4 + 1];
+            let b = pixels[i * 4 + 2];
+            pixels[i * 4] = 255 - r;
+            pixels[i * 4 + 1] = 255 - g;
+            pixels[i * 4 + 2] = 255 - b;
+        }
+        this.#ctx.putImageData(imageData, this.#ctx.canvas.width / 4, this.#ctx.canvas.height / 5);
+    }
+
     drawBrilloFilter() { }
     drawBinarizacionFilter() { }
     drawSaturacionFilter() { }
